5a6828def363cbc365e73d0dae45bcc24b1b2645
Richard Brear <richardbrear@capuk.org>
2019-10-12 16:00:07 +0100

    Restore old data set

--- server/importdata.sh
+++ server/importdata.sh
@@ -3,5 +3,5 @@
 cd "$(dirname "$0")"
 
 mongoimport --drop -d kbdb -c imd --type csv --file /var/www/data/imd/imd.csv --fieldFile=/var/www/data/imd/fieldtranslations.txt
-mongoimport --drop -d kbdb -c postcodes --type csv --file /var/www/data/postcodes/ONSPD_MAY_2019_UK.csv --headerline
+mongoimport --drop -d kbdb -c postcodes --type csv --file /var/www/data/postcodes/ONSPD_MAY_2019_UK_E.csv --headerline
 mongo --host db kbdb ./addLocationToPostcodes.js
