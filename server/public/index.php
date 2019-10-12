ff9d4a5a53472ce716d28feaf711ae1caacf8175
Iain Fogg <iainfogg@capuk.org>
2019-10-12 12:07:37 +0100

    Add super basic db connection code

--- server/public/index.php
+++ server/public/index.php
@@ -2,6 +2,17 @@
 
 declare(strict_types=1);
 
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use MongoDB\Client;
+
+$client = new Client('mongodb://db');
+$dbName = "kbdb";
+$database = $client->selectDatabase($dbName);
+
+$imdCollection = $database->selectCollection("imd");
+$postcodeCollection = $database->selectCollection("postcode");
+
 $data = ['a' => 1, 'b' => 2];
 
 $json = json_encode($data);
